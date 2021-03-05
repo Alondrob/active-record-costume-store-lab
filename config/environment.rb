b3496de896e8b618846c73b7837b54a259ee27fd
YOUR_GITHUB_USER_NAME <YOUR_GITHUB_EMAIL_ADDRESS>
2021-03-04 22:51:32 -0500

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -5,5 +5,9 @@ require "ostruct"
 require "date"
 require_all 'app/models'
 
-ENV["SINATRA_ENV"] ||= 'development'
+ENV["SINATRA_ENV"] ||= 'test'
 ActiveRecord::Base.establish_connection(ENV["SINATRA_ENV"].to_sym)
+
+# mine ends
+
+
